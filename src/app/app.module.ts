e222c9dfc14c68585ef1079e07b8e91e03581261
Crystal <crystal120599@gmail.com>
2023-12-28 21:46:20 -0800

    add todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { TodoComponent } from './components/todo/todo.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent, TodoComponent],
   imports: [
-    CommonModule
-  ]
+    BrowserModule
+  ],
+  providers: [],
+
+  bootstrap: [AppComponent]  // a component that initiates or starts the Angular application
 })
 export class AppModule { }
